554cb699e844a5fcbf8d781ebcc6e46018104f01
ayesha farooq <ayeshafarooq29052001@gmail.com>
2023-09-07 13:10:04 +0500

    Initial Commit

---
+++ Careopedia/heart_diagnosis.py
@@ -0,0 +1,12 @@
+import mysql.connector
+
+# mydb = mysql.connector.connect(
+#     host='localhost',
+#     user='root',
+#     password='xyz1234',
+#     database='careopedia')
+
+mydb=mysql.connector.connect(host="localhost",user="root",passwd="hell0w0rld!*",database="careopedia")
+
+mycursor=mydb.cursor(buffered=True)
+............
